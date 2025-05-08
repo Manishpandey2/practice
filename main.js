13c4c9b225148f353a2e6b1c7f85e7d76ede153d
Manishpande2 <biraalogang@gmail.com>
2025-05-08 15:45:46 +0545

    basic apis created

---
+++ main.js
@@ -0,0 +1,25 @@
+const app = require("express")();
+
+app.get("/hotels", (req, res) => {
+  res.json("message: hotels are fetched successfully");
+});
+app.post("/hotels", (req, res) => {
+  res.json({
+    message: "Hotel added successfully",
+  });
+});
+
+app.patch("/hotels/:id", (req, res) => {
+  res.json({
+    message: "updated successfully",
+  });
+});
+
+app.delete("/hotels/:id", (req, res) => {
+  res.json({
+    message: "Deleted successfully",
+  });
+});
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
